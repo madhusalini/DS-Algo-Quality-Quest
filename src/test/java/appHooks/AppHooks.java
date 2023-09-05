23164f23345d1e113800626631bddc3fb903bbb8
madhusalini <madhusalini93@gmail.com>
2023-09-05 09:35:47 -0400

    updated reports and screenshots

--- src/test/java/appHooks/AppHooks.java
+++ src/test/java/appHooks/AppHooks.java
@@ -1,6 +1,11 @@
 package appHooks;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -17,6 +22,7 @@ public class AppHooks {
 	private  WebDriver driver;
 	private  ConfigReader ConfigReader;
 	Properties prop;
+    private String screenshotFolder = "failed_screenshots";
 
 	@Before(order = 0)
 	public void getProperty() {
@@ -38,15 +44,34 @@ public class AppHooks {
 	}
 
 	@After(order = 1)
-	public void tearDown(Scenario scenario) {
-		if (scenario.isFailed()) {
-			// take screenshot:
-			String screenshotName = scenario.getName().replaceAll(" ", "_");
-			byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-			scenario.attach(sourcePath, "image/png", screenshotName);
+	public void captureScreenshotOnFailure(Scenario scenario) {
+        if (scenario.isFailed()) {
+            captureScreenshot(scenario.getName());
+        }
+    }
 
-		}
-        } 
+    private void captureScreenshot(String testName) {
+        try {
+            if (driver instanceof TakesScreenshot) {
+                TakesScreenshot screenshotDriver = (TakesScreenshot) driver;
+                byte[] screenshotBytes = screenshotDriver.getScreenshotAs(OutputType.BYTES);
+
+                // Create the folder if it doesn't exist
+                File folder = new File(screenshotFolder);
+                if (!folder.exists()) {
+                    folder.mkdirs();
+                }
+
+                // Define the path for the screenshot file
+                String screenshotPath = screenshotFolder + "/" + testName + ".png";
+
+                // Save the screenshot to the specified location
+                Files.write(Paths.get(screenshotPath), screenshotBytes);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
         
 }
 
